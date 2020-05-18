4648c7b198530b7df1f9d26ec4b3f8d77308607d
jharteaga <jh.arteaga09@gmail.com>
2020-05-18 10:02:01 -0700

    Add Contact Form

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function ContactUs(props) {
-  return <h1>Contact Us</h1>;
+  return (
+    <React.Fragment>
+      <h1>Contact Us</h1>
+      <Form>
+        <Form.Group controlId="formGroupFirstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter first name" />
+        </Form.Group>
+        <Form.Group controlId="formGroupLastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter last name" />
+        </Form.Group>
+        <Form.Group controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group controlId="formGroupMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows="3" placeholder="Enter message" />
+        </Form.Group>
+        <Button type="submit" size="md">
+          Send
+        </Button>
+      </Form>
+    </React.Fragment>
+  );
 }
 
 export default ContactUs;
